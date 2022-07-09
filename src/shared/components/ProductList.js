1efe752d7e35b8238c4fac1b95ce92f0bdabc1d7
djkean <djkean7@gmail.com>
2022-07-09 03:55:09 -0230

    Attempting to deploy react repo to github

--- src/shared/components/ProductList.js
+++ src/shared/components/ProductList.js
@@ -6,42 +6,54 @@ const itemImage = (itemID) =>
   `https://oldschool.runescape.wiki/images/a/a2/${itemID}`;
 
 export function Products() {
+  /* set state to empty array, then populated it with fetched data from API
+  used promises to return data in json then stored result in catalogue*/
   const [catalogue, setCatalogue] = useState([]);
+
   const fetchCatalogue = async () => {
     return await fetch(catalogueAPI)
       .then((response) => response.json())
       .then((items) => setCatalogue(items));
   };
+
   useEffect(() => {
     fetchCatalogue();
   }, []);
+
+  /* for the length of the catalogue array, i loop through each entry
+   and append a row to the table  */
   return (
-    <table className="product--list--table">
-      <thead>
-        <tr>
-          <th>Image</th>
-          <th>Item Name</th>
-          <th>Price High</th>
-          <th>Price Low</th>
-          <th>Volume High</th>
-          <th>Volume Low</th>
-        </tr>
-      </thead>
-      <tbody>
-        {catalogue.length > 0 &&
-          catalogue.map((item) => (
-            <tr>
-              <td>
-                <img src={itemImage(item.icon.replace(/ /g, "_"))} />
-              </td>
-              <td>{item.name}</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          ))}
-      </tbody>
-    </table>
+    <>
+      <section className="product--list--count">
+        There are {catalogue.length} items!
+      </section>
+      <table className="product--list--table">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Item Name</th>
+            <th>Price High</th>
+            <th>Price Low</th>
+            <th>Volume High</th>
+            <th>Volume Low</th>
+          </tr>
+        </thead>
+        <tbody>
+          {catalogue.length > 0 &&
+            catalogue.map((item) => (
+              <tr>
+                <td>
+                  <img src={itemImage(item.icon.replace(/ /g, "_"))} />
+                </td>
+                <td>{item.name}</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
   );
 }
