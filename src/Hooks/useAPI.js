31dbd421d122a746fd74f12ef8b72a6bcf987b5e
djkean <djkean7@gmail.com>
2022-10-25 08:15:38 -0230

    Refactoring of useAPI hook
    - Refactored from .then() to async/await
    - Added item api to localstorage on first attempt
    - Uses localstorage value instead of api call

--- src/Hooks/useAPI.js
+++ src/Hooks/useAPI.js
@@ -6,59 +6,52 @@ import {
 
 export const useFetchApi = () => {
   const [api, setApi] = useState([]);
-  const [localApi, setLocalApi] = useState([]);
-  const localCatalogue = JSON.parse(localStorage.getItem("localCatalogue"));
+  let localCatalogue = JSON.parse(localStorage.getItem("localCatalogue"));
 
-  const checkForLocalApi = () => {
+  // console.log(localCatalogue);
+  const checkForLocalApi2 = async () => {
     if (!localCatalogue) {
-      fetch(catalogueAPI).then((response) =>
-        response.json().then((data) => {
-          localStorage.setItem("localCatalogue", JSON.stringify(data));
-          setLocalApi(data);
-          console.log(data);
-          console.log(JSON.parse(localStorage.getItem("localCatalogue")));
-        })
-      );
-    } else {
-      return localCatalogue;
+      const itemDetailResponse = await fetch(catalogueAPI);
+      const itemDetailJSON = await itemDetailResponse.json();
+      localStorage.setItem("localCatalogue", JSON.stringify(itemDetailJSON));
+      localCatalogue = JSON.parse(localStorage.getItem("localCatalogue"));
+      // console.log(itemDetailJSON);
+      // console.log(JSON.parse(localStorage.getItem("localCatalogue")));
+      // console.log(localCatalogue);
+      return;
     }
   };
 
-  const fetchCatalogue = () => {
-    fetch(catalogueAPI)
-      .then((response) => response.json())
-      .then((items) => {
-        fetch(itemPriceAPI)
-          .then((priceJSON) => priceJSON.json())
-          .then((priceResponse) => {
-            const itemArray = Object.keys(priceResponse.data).map((key) => [
-              Number(key),
-              priceResponse.data[key],
-            ]);
-            items.map((item) => {
-              const latestPriceData = itemArray.filter(
-                (itemPairs) => itemPairs[0] === item.id
-              )[0];
-              if (latestPriceData?.length > 0) {
-                setApi((api) => ({
-                  ...api,
-                  [item.id]: {
-                    ...item,
-                    ...latestPriceData,
-                  },
-                }));
-              }
-              return api;
-            });
-          });
-      });
-    console.log(localCatalogue);
-    console.log(localApi);
+  const fetchCatalogue2 = async () => {
+    await checkForLocalApi2();
+    const itemPriceResponse = await fetch(itemPriceAPI);
+    const itemPriceJSON = await itemPriceResponse.json();
+    const itemArray = Object.keys(itemPriceJSON.data).map((key) => [
+      Number(key),
+      itemPriceJSON.data[key],
+    ]);
+    // console.log(localCatalogue, "hi");
+    localCatalogue.map((item) => {
+      const latestPriceData = itemArray.filter(
+        (matchingID) => matchingID[0] === item.id
+      )[0];
+      if (latestPriceData?.length > 0) {
+        setApi((api) => ({
+          ...api,
+          [item.id]: {
+            ...item,
+            ...latestPriceData,
+          },
+        }));
+      }
+      return api;
+    });
+    //console.log(localCatalogue);
   };
 
   useEffect(() => {
-    checkForLocalApi();
-    fetchCatalogue();
+    //checkForLocalApi2();
+    fetchCatalogue2();
   }, []);
 
   return api;
